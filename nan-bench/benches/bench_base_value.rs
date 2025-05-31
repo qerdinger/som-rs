941430604310bf87a5b2c838750622775073057d
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-05-31 10:59:37 +0100

    feat: Add create_string bench in ben base value

--- nan-bench/benches/bench_base_value.rs
+++ nan-bench/benches/bench_base_value.rs
@@ -1,28 +1,30 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use std::{hint::black_box};
+use std::{hint::black_box, time::Duration};
 use rand::random;
 use som_value::value::BaseValue;
 use som_gc::gcref::Gc;
-use som_gc::gc_interface::GCInterface;
+use som_gc::gc_interface::{GCInterface, SOMAllocator};
 use som_interpreter_bc::gc::get_callbacks_for_gc;
 use som_interpreter_bc::universe::DEFAULT_HEAP_SIZE;
 
 pub fn bench_nan_boxing(c: &mut Criterion) {
+    let mut bench_group = c.benchmark_group("nan_boxing");
+    bench_group.warm_up_time(Duration::from_millis(1));
+    bench_group.measurement_time(Duration::from_secs(1));
 
-    c.bench_function("create_string", |b| {
+    bench_group.bench_function("create_string", |b| {
         b.iter_with_setup(
             || GCInterface::init(DEFAULT_HEAP_SIZE, get_callbacks_for_gc()),
             |gc_interface| {
-                let s = black_box("Hello, World!".to_string());
-                //let gc_string: Gc<String> = gc_interface.alloc(s);
-                //let v = BaseValue::new_string(gc_string);
-                black_box(s);
+                let s = black_box("This is a string !".to_string());
+                let gc_string: Gc<String> = gc_interface.alloc(s);
+                let fstr = BaseValue::new_string(gc_string);
+                black_box(fstr);
             },
         )
     });
 
-    // Benchmark Integer creation
-    c.bench_function("create_integer", |b| {
+    bench_group.bench_function("create_integer", |b| {
         b.iter(|| {
             let val: i32 = black_box(random());
             let v = BaseValue::new_integer(val);
@@ -30,8 +32,7 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    // Benchmark Double creation
-    c.bench_function("create_double", |b| {
+    bench_group.bench_function("create_double", |b| {
         b.iter(|| {
             let val: f64 = black_box(random());
             let v = BaseValue::new_double(val);
@@ -39,8 +40,7 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    // Benchmark Boolean creation
-    c.bench_function("create_boolean", |b| {
+    bench_group.bench_function("create_boolean", |b| {
         b.iter(|| {
             let val: bool = black_box(random());
             let v = BaseValue::new_boolean(val);
@@ -48,22 +48,19 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    // Benchmark Nil creation
-    c.bench_function("create_nil", |b| {
+    bench_group.bench_function("create_nil", |b| {
         b.iter(|| {
             let v = BaseValue::NIL;
             black_box(v);
         })
     });
 
-    // Benchmarking Integer, Double, Boolean, and Nil checks
     let int_val = BaseValue::new_integer(42);
     let double_val = BaseValue::new_double(3.14);
     let bool_val = BaseValue::new_boolean(true);
     let nil_val = BaseValue::NIL;
 
-    // Benchmarking type checks
-    c.bench_function("is_integer_check", |b| {
+    bench_group.bench_function("is_integer_check", |b| {
         b.iter(|| {
             black_box(int_val.is_integer());
             black_box(double_val.is_integer());
@@ -72,7 +69,7 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("is_double_check", |b| {
+    bench_group.bench_function("is_double_check", |b| {
         b.iter(|| {
             black_box(int_val.is_double());
             black_box(double_val.is_double());
@@ -81,7 +78,7 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("is_boolean_check", |b| {
+    bench_group.bench_function("is_boolean_check", |b| {
         b.iter(|| {
             black_box(int_val.is_boolean());
             black_box(double_val.is_boolean());
@@ -90,7 +87,7 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("is_nil_check", |b| {
+    bench_group.bench_function("is_nil_check", |b| {
         b.iter(|| {
             black_box(int_val.is_nil());
             black_box(double_val.is_nil());
@@ -99,7 +96,7 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("is_ptr_type_check", |b| {
+    bench_group.bench_function("is_ptr_type_check", |b| {
         b.iter(|| {
             black_box(int_val.is_ptr_type());
             black_box(double_val.is_ptr_type());
@@ -108,33 +105,31 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    // Benchmarking extraction methods
-    c.bench_function("extract_integer", |b| {
+    bench_group.bench_function("extract_integer", |b| {
         b.iter(|| {
             black_box(int_val.as_integer());
         })
     });
 
-    c.bench_function("extract_double", |b| {
+    bench_group.bench_function("extract_double", |b| {
         b.iter(|| {
             black_box(double_val.as_double());
         })
     });
 
-    c.bench_function("extract_boolean", |b| {
+    bench_group.bench_function("extract_boolean", |b| {
         b.iter(|| {
             black_box(bool_val.as_boolean());
         })
     });
 
-    c.bench_function("extract_nil_as_boolean", |b| {
+    bench_group.bench_function("extract_nil_as_boolean", |b| {
         b.iter(|| {
             black_box(nil_val.as_boolean());
         })
     });
 
-    // Benchmarking tag extraction
-    c.bench_function("tag_extraction", |b| {
+    bench_group.bench_function("tag_extraction", |b| {
         b.iter(|| {
             black_box(int_val.tag());
             black_box(double_val.tag());
@@ -143,8 +138,7 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
         })
     });
 
-    // Benchmarking payload extraction
-    c.bench_function("payload_extraction", |b| {
+    bench_group.bench_function("payload_extraction", |b| {
         b.iter(|| {
             black_box(int_val.payload());
             black_box(double_val.payload());
@@ -152,6 +146,8 @@ pub fn bench_nan_boxing(c: &mut Criterion) {
             black_box(nil_val.payload());
         })
     });
+
+    bench_group.finish();
 }
 
 criterion_group!(benches, bench_nan_boxing);
