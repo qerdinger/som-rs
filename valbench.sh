4207d28a0d137d20abf24e4052b8f442208ab2b2
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-08-05 10:44:41 +0200

    fix: ValBench script

--- valbench.sh
+++ valbench.sh
@@ -12,6 +12,6 @@ for file in $DIRECTORY/*Bench.som; do
         #cargo run --bin ${EXE:=som-interpreter-bc} --no-default-features --features=som-gc/marksweep,idiomatic -- -c core-lib/Smalltalk core-lib/TestSuite -- TestHarness ${test_name}Test
         $EXEC -c core-lib/Smalltalk core-lib/Examples/Benchmarks core-lib/Examples/Benchmarks/LanguageFeatures core-lib/Examples/Benchmarks/TestSuite core-lib/Examples/Benchmarks/ValBenchmark . -- BenchmarkHarness ${test_name}Bench 1
         echo "-----------------------------"
-        sleep 2
+        sleep 5
     fi
 done
