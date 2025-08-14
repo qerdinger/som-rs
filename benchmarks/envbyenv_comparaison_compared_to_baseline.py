b27260d3f81d99283935cf418454b5b7619ce16f
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-08-14 22:10:06 +0200

    doc: Add benchmarks

--- benchmarks/envbyenv_comparaison_compared_to_baseline.py
+++ benchmarks/envbyenv_comparaison_compared_to_baseline.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 CSV_PATH = "som-rs-5553.csv"
 
-GLOBAL_OUT_ROOT = "output7_5553-without-imm-float_v2"
+GLOBAL_OUT_ROOT = "output7_5553-without-imm-float-micro-macro_v2"
 ENV_OUT_ROOT = os.path.join(GLOBAL_OUT_ROOT, "envs")
 SUMMARY_ROOT = os.path.join(GLOBAL_OUT_ROOT, "summaries")
 PANEL_OUT = os.path.join(GLOBAL_OUT_ROOT, "panels")
@@ -22,9 +22,9 @@ SUMMARY_IMG_DASH = os.path.join(GLOBAL_OUT_ROOT, "summary_dashboard.png")
 BASELINE_EXE = "som-rs-bc-baseline"
 
 EXCLUDE_SUITES: list[str] = [
-    # "interpreter",
-    # "macro-awfy",
-    # "somsom"
+    "interpreter",
+    "macro-awfy",
+    "somsom"
 ]
 
 SUBFOLDERS = {
